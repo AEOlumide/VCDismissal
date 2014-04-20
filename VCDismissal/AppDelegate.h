a99a5cd0d9c988af3d8e6ce8a9bdaa8401ecafdb
DOLOisSOLO <adedayo.ologunde@gmail.com>
2014-04-20 12:12:47 -0400

    Initial Commit

---
+++ VCDismissal/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VCDismissal
+//
+//  Created by Adedayo Olumide on 4/20/14.
+//  Copyright (c) 2014 Adedayo Olumide. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VCDismissal/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VCDismissal
+//
+//  Created by Adedayo Olumide on 4/20/14.
+//  Copyright (c) 2014 Adedayo Olumide. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
